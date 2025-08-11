7f2077cc67fca630c2efc167783941385abd1478
xrwong00 <22003580@siswa.um.edu.my>
2025-08-11 22:05:03 +0800

    Add prediction probability DataFrame for model output

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,4 +81,9 @@ clf.fit(X, y)
 prediction = clf.predict(input_row)
 prediction_proba = clf.predict_proba(input_row)
 
-prediction_proba
+df_prediction_proba = pd.DataFrame(prediction_proba)
+df_prediction_proba.columns = ['Adelie', 'Chinstrap', 'Gentoo']
+df_prediction_proba.rename(columns={0: 'Adelie', 
+                                    1: 'Chinstrap', 
+                                    2: 'Gentoo'})
+df_prediction_proba
