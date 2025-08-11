f869c9283d92f36a081c1dd0dfc00d5c88f12a77
xrwong00 <22003580@siswa.um.edu.my>
2025-08-11 17:06:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,21 @@ with st.expander('Data visualization'):
 with st.sidebar:
     st.header('Input features')
     island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
-    gender = st.selectbox('Gender', ('Male', 'Female'))
     bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
     bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
     flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
     body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+    gender = st.selectbox('Gender', ('Male', 'Female'))
+
+
+    # Create a DataFrame for the input features
+    data = {'island': island,
+            'bill_length_mm': bill_length_mm,
+            'bill_depth_mm': bill_depth_mm,
+            'flipper_length_mm': flipper_length_mm,
+            'body_mass_g': body_mass_g,
+            'gender': gender}
+    input_df = pd.DataFrame(data, index=[0])
+    input_penguins = pd.concat([input_df, X], axis=0)
+
+input_df
