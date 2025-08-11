c2b82c32f2a2ba61c99226aa2aa3fe35594363b4
xrwong00 <22003580@siswa.um.edu.my>
2025-08-11 21:58:57 +0800

    Fix model training by using the correct feature set for fitting

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ with st.expander('Data preparation'):
 # Model training and inference
 ## Train the ML model
 clf = RandomForestClassifier()
-clf.fit(X_raw, y)
+clf.fit(X, y)
 
 ## Apply model to make predictions
 prediction = clf.predict(input_row)
